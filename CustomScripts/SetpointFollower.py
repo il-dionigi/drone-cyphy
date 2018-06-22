1360b4444a2d7971c8ad240649e71b91f7d63c0d
TL <>
2018-06-22 16:50:19 -0700

    Slight Modification to code v3

--- CustomScripts/SetpointFollower.py
+++ CustomScripts/SetpointFollower.py
@@ -349,12 +349,12 @@ def follow_paths(scf):
 		t = T
 		if (position[0]/T > VMAX or position[1]/T > VMAX):
 			t = position[0]/VMAX if position[0]/VMAX > position[1]/VMAX else position[1]/VMAX 
-            go_straight_d(cf, movement[0], movement[1], movement[2], t)
-            time.sleep(1)
-            print('At pos: ({}, {}, {})'.format(position_internal[0], position_internal[1], position_internal[2]))
-        for i in range(4):
-            position_internal[i] = movement[i]
-        time.sleep(0.1)
+			go_straight_d(cf, movement[0], movement[1], movement[2], t)
+			time.sleep(1)
+			print('At pos: ({}, {}, {})'.format(position_internal[0], position_internal[1], position_internal[2]))
+		for i in range(4):
+			position_internal[i] = movement[i]
+		time.sleep(0.1)
 
 def csv_stab(timestamp, data, self):
 	global stab_writer
