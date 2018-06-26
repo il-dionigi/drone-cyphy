edbf6f0516285d069c8a843723b2a6c631ffd17f
TL <>
2018-06-26 13:39:18 -0700

    minor fixes

--- CustomScripts/3dGrapher.py
+++ CustomScripts/3dGrapher.py
@@ -10,12 +10,6 @@ ax = p3.Axes3D(fig)
 steps = 100
 data = []
 
-def gen(n):
-    phi = 0
-    while phi < 2*np.pi:
-        yield np.array([np.cos(phi), np.sin(phi), phi])
-        phi += 2*np.pi/n
-
 def update(num, data, line):
     line.set_data(data[:2, :num])
     line.set_3d_properties(data[2, :num])
@@ -25,12 +19,13 @@ with open(sys.argv[0], 'rb') as csvfile:
     posreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     l = (len(posreader))
     l = range(l)[0::int(l/steps)]
-    for i in l:
-        row = posreader[i];
+    i = 0
+    for row in posreader:
         if i == 0:
             print(row)
-        else:
+        else if (i in l):
             data.append([row[1], row[2], row[3]])
+        i++
 N = len(data)
 line, = ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1])
 
