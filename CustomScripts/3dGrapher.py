8ab1c337700c51d0975352eb4860528931e47884
TL <>
2018-06-26 13:32:00 -0700

    changed 3dgrapher

--- CustomScripts/3dGrapher.py
+++ CustomScripts/3dGrapher.py
@@ -2,9 +2,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 import mpl_toolkits.mplot3d.axes3d as p3
 from matplotlib import animation
+import csv
 
 fig = plt.figure()
 ax = p3.Axes3D(fig)
+steps = 100
+data = []
 
 def gen(n):
     phi = 0
@@ -16,18 +19,28 @@ def update(num, data, line):
     line.set_data(data[:2, :num])
     line.set_3d_properties(data[2, :num])
 
-N = 100
-data = np.array(list(gen(N))).T
+
+with open('PUT NAME HERE BRIAN.csv', 'rb') as csvfile:
+    posreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    l = (len(posreader))
+    l = range(l)[0::int(l/steps)]
+    for i in l:
+        row = posreader[i];
+        if i == 0:
+            #title stuff, can do something later?
+        else:
+            data.append([row[1], row[2], row[3]])
+N = len(data)
 line, = ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1])
 
 # Setting the axes properties
-ax.set_xlim3d([-1.0, 1.0])
+ax.set_xlim3d([-2, 2])
 ax.set_xlabel('X')
 
-ax.set_ylim3d([-1.0, 1.0])
+ax.set_ylim3d([-2, 2])
 ax.set_ylabel('Y')
 
-ax.set_zlim3d([0.0, 10.0])
+ax.set_zlim3d([0, 2])
 ax.set_zlabel('Z')
 
 ani = animation.FuncAnimation(fig, update, N, fargs=(data, line), interval=10000/N, blit=False)
